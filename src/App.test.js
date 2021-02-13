cc7900278cbdd2e97a3b741c7da8c4638c0336a1
Beatriz Sandoval <mbsandoval10@gmail.com>
2021-02-13 15:04:32 -0300

    fixed app test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders login screen", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const toggleElement = getByText("Don't have an account?");
+  expect(toggleElement).toBeInTheDocument();
 });
